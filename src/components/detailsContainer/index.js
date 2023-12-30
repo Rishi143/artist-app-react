076e8d979c2fe97bcd9c7a3f5fa7e6f9c0a5c002
rishi143 <rishikeshreddykarnati@gmail.com>
2023-12-30 16:06:31 +0530

    Added Lyrics for first song

--- src/components/detailsContainer/index.js
+++ src/components/detailsContainer/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid } from '@mui/material';
+import { Context } from '../../store';
 
 export const DetailsContainer = () => {
+    const [data, dispatch] = useContext(Context);
     return (
         <Grid item xs="auto" sm={9} className={'artistDetailsContainer'} style={{ minHeight: '93vh', padding: 16 }}>
-            <div style={{ backgroundColor: '#fff', minHeight: '87vh', borderRadius: 10, padding: 10 }}>DetailsContainer</div>
+            {data.artists.length ? data.artists.filter(artist => data.artistId == artist.id).map(artist => 
+                <div style={{ backgroundColor: '#fff', minHeight: '87vh', borderRadius: 10, padding: 10 }} className='lyricsContainer'>
+                    {artist.lyrics? artist.lyrics[0] : "No Lyrics Found For Artist"}
+                </div>
+            ) : "No Artist Found"}
         </Grid>
     )
 }
--- src/mocks.js
+++ src/mocks.js
@@ -1,9 +1,93 @@
+import React from "react";
+
 export const artists = [
     {
         name: 'sidsriram',
         nationality: 'indian',
         age: 25,
-        id: '123'
+        id: '123',
+        lyrics: [
+            <pre>You’re My Sunshine<br/>
+                You’re My Moonlight<br/>
+                You’re Star In The Sky<br/>
+                Come With Me Now<br/>
+
+                You Have My Desire<br/>
+                Naatho Ra Neela Ra<br/>
+                Aradhya<br/>
+
+                Padhamu Nee Vaipilaa<br/>
+                Parugu Needhe Kadhaa<br/>
+                Thanuvu Thera Meedhuga<br/>
+                Cheruko Twaragaa<br/>
+
+                Manasara Chelithara<br/>
+                Na Gundeni Motham Thavvi Thavva<br/>
+                Chandhanamantha Challaga Dhochave<br/>
+                Ye Vandhala Koddhi Pandaga Lunna<br/>
+                Vennela Motham Ninduga Unna<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Ee Poota Naa Paata<br/>
+                Cheraali Nee Dhaaka<br/>
+                Nee Chinni Meda Vampule<br/>
+
+                Saagali Ee Aata<br/>
+                Thedaalu Thelaaka<br/>
+                Gelichedhi Yevaremito<br/>
+
+                Ilaage Untaale<br/>
+                Neethone Dhooraale<br/>
+                Theeraalu Levu<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Yedho Anaalandhi<br/>
+                Inka Vinaalandhi<br/>
+                Nee Ooha Mallimpule<br/>
+
+                Naa Dhaaka Cherindhi<br/>
+                Naa Kuda Baagundhi<br/>
+                Nee Prema Kavvimpulo<br/>
+
+                Nee Laane<br/>
+                Maraane<br/>
+                Antaane<br/>
+
+                Nuvvantu Nenantu Leme<br/>
+
+                Manasara Chelithara<br/>
+                Na Gundeni Motham Thavvi Thavvi<br/>
+                Chandhanamantha Challaga Dhochave<br/>
+                Ye Vandhala Koddhi Pandaga Lunna<br/>
+                Vennela Motham Ninduga Unna<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Aradhya Na Aradhya<br/>
+                Nuvve Lenidhedhi Vaddhu<br/>
+                Aradhya<br/>
+
+                Padhamu Nee Vaipilaa<br/>
+                Parugu Needhe Kadhaa<br/>
+                Thanuvu Thera Meedhuga<br/>
+                Cheruko Twaragaa<br/></pre>
+        ]
     },
     {
         name: 'sidsriram',
